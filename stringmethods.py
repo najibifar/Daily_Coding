b30f6460faa06822113cf76197d286cc5c02f8ba
Amir <99111537+najibifar@users.noreply.github.com>
2025-01-28 22:43:41 +0330

    1-28-2025

--- stringmethods.py
+++ stringmethods.py
@@ -3,3 +3,13 @@ print(a.capitalize())
 print(a.casefold())
 print(a.center(100))
 print(a.count("co"))
+
+
+print('Hello', 'World')
+x = 'Welcome'
+y = 'Coders'
+print(x + y)
+
+price = 1000
+txt = f'The price is {price:,} dollars'
+print(txt)
