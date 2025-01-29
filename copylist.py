f64ac58d9a4009d1297d702c8e7eeb7e4207acb8
Amir <99111537+najibifar@users.noreply.github.com>
2025-01-29 11:21:20 +0330

    1-29-2025

---
+++ copylist.py
@@ -0,0 +1,35 @@
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[1:2]
+print(mylist)
+
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
+
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
