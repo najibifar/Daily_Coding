a8c47a96d6d19bf1127fa1c7ef07db11c9e7e2c0
Amir <99111537+najibifar@users.noreply.github.com>
2025-02-03 22:56:48 +0330

    2-03-2025

---
+++ txt.py
@@ -0,0 +1,12 @@
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+a = "amir"
+print(a.upper())
+
+
+a = " Hello, World! "
+print(a.strip())
