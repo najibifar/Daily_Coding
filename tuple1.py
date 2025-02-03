9535d194adebeba50de5713acda315aec3039cd8
Amir <99111537+najibifar@users.noreply.github.com>
2025-02-03 22:26:09 +0330

    2-02-2025

--- tuple1.py
+++ tuple1.py
@@ -13,3 +13,15 @@ i = 0
 while i < len(thistuple):
   print(thistuple[i])
   i = i + 1
+
+
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+
+thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+x = thistuple.count(5)
+print(x)
