af910aa4dfd247068432e64d4048a602a257a3de
Amir <99111537+najibifar@users.noreply.github.com>
2025-02-09 09:21:16 +0330

    2-09-2025

--- function1.py
+++ function1.py
@@ -19,3 +19,22 @@ def my_function(x):
 print(my_function(3))
 print(my_function(5))
 print(my_function(9))
+
+
+
+x = lambda a : a + 10
+print(x(5))
+
+x = lambda a, b : a * b
+print(x(5, 6))
+
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
+
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+
+print(mydoubler(11))
