df0372eefb886582942959f4aba8eb84668c01a9
Amir <99111537+najibifar@users.noreply.github.com>
2025-02-12 21:47:03 +0330

    2-12-2025

--- datatime.py
+++ datatime.py
@@ -1,4 +1,16 @@
 import datetime
-
 x = datetime.datetime.now()
 print(x)
+
+x = datetime.datetime.now()
+print(x.year)
+print(x.strftime("%A"))
+
+x = datetime.datetime(2020, 5, 17)
+print(x)
+
+x = datetime.datetime(2018, 6, 1)
+print(x.strftime("%B"))
+
+x = datetime.datetime(2024, 8, 20)
+print(x.strftime('%d'))
