8e7def4dea519fe5f4dcf65fc041386f57169383
Amir <99111537+najibifar@users.noreply.github.com>
2025-02-14 13:49:21 +0330

    2-14-2025

--- except1.py
+++ except1.py
@@ -4,3 +4,18 @@ except NameError:
   print("Variable x is not defined")
 except:
   print("Something else went wrong")
+
+
+try:
+  print("Hello")
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")
+
+try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
