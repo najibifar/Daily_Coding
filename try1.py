8c0f116765921c67f96e63741609b5a8c7e595a2
Amir <99111537+najibifar@users.noreply.github.com>
2025-02-14 13:09:56 +0330

    2-14-2025

--- try1.py
+++ try1.py
@@ -2,3 +2,11 @@ try:
   print(x)
 except:
   print("An exception occurred")
+
+
+try:
+  print(x)
+except NameError:
+  print("Variable x is not defined")
+except:
+  print("Something else went wrong")
