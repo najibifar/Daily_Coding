68f2d05da421a7bc7993ba02fb7c265f4d77d444
Amir <99111537+najibifar@users.noreply.github.com>
2025-02-14 14:01:50 +0330

    2-14-2025

--- file1.py
+++ file1.py
@@ -5,6 +5,9 @@ print(f.readline())
 for x in f:
     print(x)
 
+f = open("demofile.txt", "a")
+f.write("Now the file has more content!")
+f.close()
 
 f = open("I:\welcome.txt", "r")
 print(f.read())
---
+++ file2.py
@@ -0,0 +1,5 @@
+f = open("demofile.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+f = open("demofile.txt", "r")
+print(f.read())
