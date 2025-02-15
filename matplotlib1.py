2236e7e2e26aa24b8979e7ada869b59cd8113e67
Amir <99111537+najibifar@users.noreply.github.com>
2025-02-15 22:24:59 +0330

    2-15-2025

--- matplotlib1.py
+++ matplotlib1.py
@@ -1,2 +1,29 @@
 import matplotlib
 print(matplotlib.__version__)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints, 'o')
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10, 5, 7])
+
+plt.plot(ypoints)
+plt.show()
