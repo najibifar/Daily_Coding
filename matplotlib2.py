935dc4f601db5d1b1cdec05dd24fb8fa97369eab
Amir <99111537+najibifar@users.noreply.github.com>
2025-02-16 22:46:12 +0330

    Update matplotlib2.py

--- matplotlib2.py
+++ matplotlib2.py
@@ -5,3 +5,11 @@ ypoints = np.array([3, 8, 1, 10])
 
 plt.plot(ypoints, marker = 'H')
 plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker = 'o', ms = 20, mec = 'r', mfc = 'r')
+plt.show()
