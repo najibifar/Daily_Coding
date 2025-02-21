a058fb01e10da1792d7b372bae833a8f9f67dea5
Amir <99111537+najibifar@users.noreply.github.com>
2025-02-21 20:38:42 +0330

    2-21-2025

--- sinosi.py
+++ sinosi.py
@@ -1,9 +1,14 @@
 import numpy as np
+import geopandas as gpd
 import matplotlib.pyplot as plt
 
 x = np.linspace(0, 10, 100)
 y = np.sin(x)
-
-
 plt.plot(x, y)
+plt.show()
+
+wrold = gpd.read_file('ne_110m_admin_0_countries.shp')
+
+wrold.plot(edgecolor = 'pink')
+plt.title("world map with country borders")
 plt.show()
