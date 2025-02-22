d3337c5613298d3185c541b58e7c5bb45cdafbce
Amir <99111537+najibifar@users.noreply.github.com>
2025-02-22 22:24:03 +0330

    2-22-2025

--- daily.py
+++ daily.py
@@ -13,3 +13,17 @@ print(s)
 
 x = "Python" 
 print(x[2:5])
+
+
+set1 = {10, 20, 30} 
+set2 = set1.remove(20) 
+print(set2)
+
+def extend_list(lst): 
+    lst.extend([10]) 
+    b = [5, 10, 15, 20] 
+    extend_list(b)
+print(len(b))
+
+queue = {'name', 'age', 'DOB'} 
+print(queue) 
