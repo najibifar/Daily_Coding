b042340e36cfcf31861682df270452e084772782
Amir <99111537+najibifar@users.noreply.github.com>
2025-03-01 18:32:53 +0330

    03-01-2025

--- numpy1.py
+++ numpy1.py
@@ -5,3 +5,12 @@ x = numpy.random.normal(5.0, 1.0, 100000)
 
 plt.hist(x, 100)
 plt.show()
+
+
+import matplotlib.pyplot as plt
+
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+plt.scatter(x, y)
+plt.show()
