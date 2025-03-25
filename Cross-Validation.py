f02fd873f8b4b37db7d23f6dc53476eab82282ab
Amir <99111537+najibifar@users.noreply.github.com>
2025-03-25 19:50:30 +0330

    03-25-2025

--- Cross-Validation.py
+++ Cross-Validation.py
@@ -3,31 +3,32 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import KFold, cross_val_score
 
 X, y = datasets.load_iris(return_X_y=True)
-
 clf = DecisionTreeClassifier(random_state=42)
-
 k_folds = KFold(n_splits = 5)
-
 scores = cross_val_score(clf, X, y, cv = k_folds)
-
 print("Cross Validation Scores: ", scores)
 print("Average CV Score: ", scores.mean())
 print("Number of CV Scores used in Average: ", len(scores))
 
 
 
-from sklearn import datasets
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import StratifiedKFold, cross_val_score
 
 X, y = datasets.load_iris(return_X_y=True)
-
 clf = DecisionTreeClassifier(random_state=42)
-
 sk_folds = StratifiedKFold(n_splits = 5)
-
 scores = cross_val_score(clf, X, y, cv = sk_folds)
+print("Cross Validation Scores: ", scores)
+print("Average CV Score: ", scores.mean())
+print("Number of CV Scores used in Average: ", len(scores))
 
+
+from sklearn.model_selection import LeaveOneOut, cross_val_score
+
+X, y = datasets.load_iris(return_X_y=True)
+clf = DecisionTreeClassifier(random_state=42)
+loo = LeaveOneOut()
+scores = cross_val_score(clf, X, y, cv = loo)
 print("Cross Validation Scores: ", scores)
 print("Average CV Score: ", scores.mean())
 print("Number of CV Scores used in Average: ", len(scores))
