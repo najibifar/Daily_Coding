c7fbf1d65ae874d10d49682546fc4468a4835f54
Amir <99111537+najibifar@users.noreply.github.com>
2025-03-26 21:26:45 +0330

    03-26-2025

--- auc-roc.py
+++ auc-roc.py
@@ -18,3 +18,19 @@ print('Confusion matrix')
 print(cf_mat)
 print(f'class 0 accuracy: {cf_mat[0][0]/n_0}')
 print(f'class 1 accuracy: {cf_mat[1][1]/n_1}')
+
+
+
+# below are the probabilities obtained from a hypothetical model that doesn't always predict the mode
+y_proba_2 = np.array(
+    np.random.uniform(0, .7, n_0).tolist() +
+    np.random.uniform(.3, 1, n_1).tolist()
+)
+y_pred_2 = y_proba_2 > .5
+
+print(f'accuracy score: {accuracy_score(y, y_pred_2)}')
+cf_mat = confusion_matrix(y, y_pred_2)
+print('Confusion matrix')
+print(cf_mat)
+print(f'class 0 accuracy: {cf_mat[0][0]/n_0}')
+print(f'class 1 accuracy: {cf_mat[1][1]/n_1}')
