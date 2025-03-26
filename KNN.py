6c7e066133190e49a9f73f53bb444bd8bd947554
Amir <99111537+najibifar@users.noreply.github.com>
2025-03-26 21:30:01 +0330

    03-26-2025

--- KNN.py
+++ KNN.py
@@ -6,3 +6,14 @@ classes = [0, 0, 1, 0, 0, 1, 1, 0, 1, 1]
 
 plt.scatter(x, y, c=classes)
 plt.show()
+
+
+new_x = 8
+new_y = 21
+new_point = [(new_x, new_y)]
+
+prediction = knn.predict(new_point)
+
+plt.scatter(x + [new_x], y + [new_y], c=classes + [prediction[0]])
+plt.text(x=new_x-1.7, y=new_y-0.7, s=f"new point, class: {prediction[0]}")
+plt.show()
