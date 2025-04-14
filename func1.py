fcbee35a95d98bea10b829ad891c30fd953cd584
Amir <99111537+najibifar@users.noreply.github.com>
2025-04-14 22:38:46 +0330

    04-14-2025

--- func1.py
+++ func1.py
@@ -1,3 +1,12 @@
 def func(Tuple):
     return Tuple
 print(func((1, 2, 3)) * 2)
+
+
+List = [[1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16]]
+
+for i in range(0,4):
+    print(List[i][1], end="/")
